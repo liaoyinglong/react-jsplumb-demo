8237cd9e7505ecc72fcb8cd04e2dc46d3bddd960
liaoyinglong <vigossliao@gmail.com>
2019-03-22 18:13:36 +0800

    refactor: 变量名优化

--- src/shared-styled/Layout.tsx
+++ src/shared-styled/Layout.tsx
@@ -3,30 +3,28 @@ import styled from "styled-components";
 export const DiagramContainer = styled.div`
   padding: 20px;
   width: 80%;
-  height: 200px;
-  border: 1px solid gray;
   margin-bottom: 20px;
   margin-top: 10px;
 `;
 
-interface IItemProps {
+interface ItemProps {
   draggable?: boolean;
 }
 
-export const Item = styled.div<IItemProps>`
+export const Item = styled.div<ItemProps>`
   height: 80px;
   width: 80px;
   border: 1px solid blue;
   float: left;
-  ${(props: IItemProps) => (props.draggable ? " position:absolute" : "")};
+  ${(props: ItemProps) => (props.draggable ? " position:absolute" : "")};
 `;
 
 export const ItemRight = styled(Item)`
   margin-left: 50px;
-  ${(props: IItemProps) => (props.draggable ? "left: 150px" : "")};
+  ${(props: ItemProps) => (props.draggable ? "left: 150px" : "")};
 `;
 
 export const ItemBottom = styled(Item)`
   margin-top: 80px;
-  ${(props: IItemProps) => (props.draggable ? "bottom: 150px" : "")};
+  ${(props: ItemProps) => (props.draggable ? "bottom: 150px" : "")};
 `;
