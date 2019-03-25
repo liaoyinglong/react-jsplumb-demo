dc4282715c0d47ccdf34917d253c1999de952567
liaoyinglong <vigossliao@gmail.com>
2019-03-25 16:03:26 +0800

    refactor: 简化jsx嵌套

--- src/pages/editor/components/LeftContainer.tsx
+++ src/pages/editor/components/LeftContainer.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import { BothSideStyled } from "src/pages/editor/components/shared";
 import * as React from "react";
+import { useState } from "react";
 import { Button } from "antd";
+import { ButtonProps } from "antd/lib/button";
 
 const Wrapper = styled(BothSideStyled).attrs({
   className: "LeftContainer"
@@ -13,18 +15,46 @@ const Wrapper = styled(BothSideStyled).attrs({
   flex-direction: column;
 `;
 
-const OperationBtn = styled(Button)`
+const OperationBtn = styled(Button)<ButtonProps>`
   margin: 0 0 10px;
 `;
 
+/**
+ * 操作按钮枚举
+ */
+export enum operationType {
+  select = "选择",
+  start = "开始",
+  task = "任务",
+  cond = "条件",
+  subflow = "子流",
+  link = "连线",
+  end = "结束"
+}
+
+const operationArr = [
+  operationType.start,
+  operationType.task,
+  operationType.cond,
+  operationType.subflow,
+  operationType.end
+];
+
 export const LeftContainer = () => {
+  const [type, setType] = useState(operationType.select);
   return (
     <Wrapper>
-      <OperationBtn>开始</OperationBtn>
-      <OperationBtn>任务</OperationBtn>
-      <OperationBtn>条件</OperationBtn>
-      <OperationBtn>子流程</OperationBtn>
-      <OperationBtn>结束</OperationBtn>
+      <div>
+        当前操作项:
+        <br />
+        {type}
+      </div>
+
+      {operationArr.map(item => (
+        <OperationBtn onClick={() => setType(item)} key={item}>
+          {item}
+        </OperationBtn>
+      ))}
     </Wrapper>
   );
 };
